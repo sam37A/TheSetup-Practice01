6f7d189e4f37beb79ae1227815eb8c9711804d40
sam37A <sahilsan30@gmail.com>
2022-11-17 07:41:04 -0800

    aabout and contact templet added

---
+++ aboutpage.php
@@ -0,0 +1,16 @@
+<?php  
+/**  
+* Template Name: About
+*  
+* @package TheSetup-Practice01
+ 
+*  
+*/  
+?>  
+<?php get_header(); ?>
+<!-- code goes here -->
+<h1>About Page</h1>
+
+
+
+<?php get_footer();?>
